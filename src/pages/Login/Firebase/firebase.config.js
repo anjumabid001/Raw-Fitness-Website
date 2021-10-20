5260a4e41a439a117c0fe2c418d6f414f05c6ee2
anjumabid001 <anjumpust001@gmail.com>
2021-10-20 23:28:43 +0600

    final commit

---
+++ src/pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDPXSr-__tMe6G73C-axbDDQcLzvgKzxOM",
+  authDomain: "raw-fitness-6a5fc.firebaseapp.com",
+  projectId: "raw-fitness-6a5fc",
+  storageBucket: "raw-fitness-6a5fc.appspot.com",
+  messagingSenderId: "112035986377",
+  appId: "1:112035986377:web:972fa4a6feb588a37b8618"
+};
+
+export default firebaseConfig;
